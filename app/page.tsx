ce07c8730c6fdb0d70f275d95e55822b5890bacd
Mohamed Hafez <mhafez1978@outlook.com>
2023-06-17 01:11:29 -0400

    skelaton

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import Link from "next/link";
-export default function Home() {
-  return (
-    <div className="main">
-      <Link href="/about">About</Link>
-    </div>
-  );
-}
